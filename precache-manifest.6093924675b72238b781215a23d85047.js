4ba221c7329a2b0c0b12b81592ebc13041e55ad5
awesome-button <oleshko.lena@gmail.com>
2020-10-17 18:07:06 +0200

    Updates

---
+++ precache-manifest.6093924675b72238b781215a23d85047.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c10992663ac9f270d95bb8e55a89a650",
+    "url": "/Countries-Info-with-currency-converter/index.html"
+  },
+  {
+    "revision": "4cdbacb6e04b2efbb31b",
+    "url": "/Countries-Info-with-currency-converter/static/css/main.3bc8d576.chunk.css"
+  },
+  {
+    "revision": "daff9619540bd9905cd6",
+    "url": "/Countries-Info-with-currency-converter/static/js/2.f0d378a2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Countries-Info-with-currency-converter/static/js/2.f0d378a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cdbacb6e04b2efbb31b",
+    "url": "/Countries-Info-with-currency-converter/static/js/main.7564e69b.chunk.js"
+  },
+  {
+    "revision": "f9f748ecf413a3a62b00",
+    "url": "/Countries-Info-with-currency-converter/static/js/runtime-main.98ba5599.js"
+  },
+  {
+    "revision": "15d0cb72c8bbd6f0feee300f045060d0",
+    "url": "/Countries-Info-with-currency-converter/static/media/bg3.15d0cb72.png"
+  }
+]);
